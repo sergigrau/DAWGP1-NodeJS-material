b7f13754bf95adae59d86cd32aec8f642f80d9b9
sergi grau <sergi.grau@fje.edu>
2021-11-03 10:53:25 +0100

    v2

--- M01_consola.js
+++ M01_consola.js
@@ -2,14 +2,14 @@
  * exemple bàsic de treball amb  Node JS
  * @author sergi grau, sergi.grau@fje.edu
  * @version 1.0
- * date 14.02.2014
+ * date 14.02.2014 
  * format del document UTF-8
  *
  * CHANGELOG
  * 08.10.2015
  * - Servidor HTTP bàsic amb Node JS
  *
- * NOTES
+ * NOTES  
  * ORIGEN
  * Desenvolupament Aplicacions Web. Jesuïtes El Clot
  */
